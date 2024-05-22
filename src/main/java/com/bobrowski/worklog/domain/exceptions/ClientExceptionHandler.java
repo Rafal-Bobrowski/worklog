87e4a415a106284be188efbdfc5b3a2331d03262
Rafal-Bobrowski <rafal.bobrowski95@gmail.com>
2024-05-22 13:04:41 +0200

    Fix request validation, handle exception when invalid requestBody.

--- src/main/java/com/bobrowski/worklog/domain/exceptions/ClientExceptionHandler.java
+++ src/main/java/com/bobrowski/worklog/domain/exceptions/ClientExceptionHandler.java
@@ -3,7 +3,12 @@ package com.bobrowski.worklog.domain.exceptions;
 import com.bobrowski.worklog.openapi.model.ExceptionMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -20,5 +25,21 @@ public class ClientExceptionHandler extends ResponseEntityExceptionHandler {
                 .code(4041)
                 .build();
         return handleExceptionInternal(ex, exceptionMessage, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, @NonNull HttpHeaders headers, @NonNull HttpStatusCode status, @NonNull WebRequest request) {
+        ObjectError objectError = ex.getBindingResult().getAllErrors().get(0);
+        String message;
+        if (objectError instanceof FieldError fieldError){
+            message = "Property with name: " + fieldError.getField() + " in object: " + fieldError.getObjectName() + " cannot be: " + fieldError.getRejectedValue();
+            ExceptionMessage exceptionMessage = ExceptionMessage.builder()
+                    .responseCode(HttpStatus.BAD_REQUEST.value())
+                    .message(message)
+                    .code(4041)
+                    .build();
+            return handleExceptionInternal(ex, exceptionMessage, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        }
+        return super.handleMethodArgumentNotValid(ex, headers, status, request);
     }
 }
